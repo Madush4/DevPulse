ac2afcae5b84221df84f62411502c1b311add10e
Madusha Damith <madushdamith8@gmail.com>
2026-07-11 20:43:20 +0530

    feat: setup frontend routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import { Route, Routes } from "react-router";
+import Home from "../pages/Home";
+import Profile from "../pages/Profile";
+import "./App.css";
 
 function App() {
- 
-
   return (
     <div>
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/u/:username" element={<Profile />} />
+      </Routes>
+      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
