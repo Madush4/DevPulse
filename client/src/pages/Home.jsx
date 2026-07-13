272aaecd682241d90542b50c7664006b1f93086c
Madusha Damith <madushdamith8@gmail.com>
2026-07-13 16:16:18 +0530

    chore: Add footer to Home.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
-import React , {useState} from 'react'
-import Navbar from '../components/layout/Navbar'
+import React , {useState} from 'react';
+import Navbar from '../components/layout/Navbar';
+import Footer from '../components/layout/Footer';
 
 function Home() {
   return (
     <div className='bg-slate-800'>
       <Navbar />
       <h1>Home</h1>
+      <Footer/>
     </div>
   );
 }
