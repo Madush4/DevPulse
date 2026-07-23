cab944f7d3d97fd50396000303215fea0770dc67
Madusha Damith <madushdamith8@gmail.com>
2026-07-23 23:58:45 +0530

    feat: Add search bar to home page

---
+++ client/src/components/ui/searchBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function SearchBar() {
+  return (
+    <div>
+        <input className='p-4 bg-white text-gray-900 rounded-lg'  placeholder='Enter a name'/>
+
+    </div>
+  )
+}
+
+export default SearchBar
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React , {useState} from 'react';
 import Navbar from '../components/layout/Navbar';
 import Footer from '../components/layout/Footer';
+import SearchBar from '../components/ui/searchBar';
 
 function Home() {
   return (
@@ -14,6 +15,8 @@ function Home() {
         <h2 className='text-2xl md:text-4xl lg:text-5xl'>Understand Any Developer </h2>
         <h3 className='text-indigo-500 text-lg md:text-2xl lg:text-4xl'>At Glance</h3>
 
+        <p className='text-gray-500 w-'>Type a GitHub username. Get beautiful dashboard of their activity, streaks, languages, repo - ad an AI-generated developer profile. </p>
+        <SearchBar />
 
       </section>
       <Footer/>
